adda1e9158468eda7d9fdea5c11e90930736b14a
Thaisgms <thais.gomes@sptech.school>
2025-05-19 23:09:30 -0300

    Arquivos com correções e melhorias

---
+++ site/assets/js/tiana.js
@@ -0,0 +1,10 @@
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        const body = accordion.querySelector('.accordion-body');
+        body.classList.toggle('active')
+    })
+})
+
+
