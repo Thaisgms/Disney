bd16c86d29bb001a6a86cc9a37a8fb601852eaba
Thaisgms <thais.gomes@sptech.school>
2025-05-30 11:05:13 -0300

    novas telas e atualizações

---
+++ public/assets/js/js.js
@@ -0,0 +1,37 @@
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        const body = accordion.querySelector('.accordion-body');
+        body.classList.toggle('active')
+    })
+})
+
+const swiper = new Swiper('.personagens', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
